63bcd1759948034a7886e14733ebed9ad3674645
ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2026-07-15 16:07:09 +0200

    last unit one challlenge

---
+++ Unit1/ticket_booker.py
@@ -0,0 +1,10 @@
+#concert ticket booking system
+
+
+#---collect input------
+
+name = input("Enter your name: ")
+artist = input("enter the name of the band/artist you want to see: ")
+
+#----confirmation message------
+print(f"Hey {name}, your tickets to see {artist} have been booked successfully! Enjoy the show!")
