2c199cf426d8c9ff08bdaf19075d402369e89d16
ziphozenkosimthombe <ziphoncayiyana@gmail.com>
2026-07-26 20:54:35 +0200

    numeric data

---
+++ Unit3/working_with_numeric_data.py
@@ -0,0 +1,16 @@
+#Arithmetic operations:
+#Addition: 3 + 2
+#Subtraction: 3 - 2
+#Multiplication: 3 * 2
+#Division: 3 / 2
+#floor division: 3 // 2
+#exponentiation: 3 ** 2
+#modulus: 3 % 2
+
+print("Addition: ", 3 + 2)
+print("Subtraction: ", 3 - 2)
+print("Multiplication: ", 3 * 2)
+print("Division: ", 3 / 2)
+print("Floor Division: ", 3 // 2)
+print("Exponentiation: ", 3 ** 2)
+print("Modulus: ", 3 % 2)
